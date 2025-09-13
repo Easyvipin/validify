c57d1a06c9d8ef9a87470ac01720216533f3eceb
easyvipin <vipinchandra555@gmail.com>
2025-09-13 23:58:47 +0530

    added react bit shiny component

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,3 +1,4 @@
+import ShinyText from "@/components/ShinyText";
 import { isUserLoggedIn } from "../action";
 import AuthRedirectButton from "@/components/AuthRedirectButton";
 
@@ -5,6 +6,19 @@ export default async function Home() {
   const isLoggedIn = await isUserLoggedIn();
   return (
     <div className="font-sans bg-background/95 grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20">
+      <ShinyText
+        text="Early Access"
+        disabled={false}
+        speed={3}
+        className="custom-class text-8xl"
+      />
+      <ShinyText
+        text="To Tomorrow’s Projects"
+        disabled={false}
+        speed={3}
+        className="custom-class text-8xl"
+      />
+      <h3>Early Birds , MVP , or college side projects</h3>
       <AuthRedirectButton
         isAuthenticated={isLoggedIn}
         label="Validate your project"
